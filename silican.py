156a0c099bd97a86f0c9b7016a81b5eff388a80d
Tomasz Zdanowski <zdanozdan@MacBook-2.local>
2021-11-28 20:05:28 +0100

    added silican thread

---
+++ silican.py
@@ -0,0 +1,88 @@
+from PyQt5.QtCore import QThread, pyqtSignal,Qt
+import db,config
+import time
+import xml.etree.ElementTree as ET
+import sys,socket,time
+
+class SilicanThreadBase(QThread):
+    _signal = pyqtSignal(tuple)
+    
+    def __init__(self,parent=None):
+        super(SilicanThreadBase, self).__init__(parent=parent)
+        
+    def __del__(self):
+        self.wait()
+
+    def connect(self):
+        self.config = db.load_config()
+        try:
+            self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            self.sock.settimeout(10)
+            self.sock.connect((self.config['silican_address'],self.config['silican_port']))
+            self.sock.settimeout(None)
+            self._signal.emit((config.SILICAN_CONNECTED,"OK"))
+        except socket.timeout as e:
+            self._signal.emit((config.SILICAN_ERROR,str(e)))
+        except socket.error as e:
+            self._signal.emit((config.SILICAN_ERROR,str(e)))
+            #raise
+
+    def login(self):
+        message = '<XCTIP><Log><MakeLog><CId>12</CId><Login>%s</Login><Pass>%s</Pass></MakeLog></Log></XCTIP>' % (self.config['login'],self.config['password'])
+        self.sock.sendall(message.encode('UTF-8'))
+
+    def register_req(self):
+        message = b'<XCTIP><Calls><Register_REQ><CId>1</CId><Id>1001</Id><Pass>mikran123</Pass></Register_REQ></Calls></XCTIP>'
+        #message = b'<XCTIP><Calls><Register_REQ><CId>1</CId></Register_REQ></Calls></XCTIP>'
+        self.sock.sendall(message)
+
+    def read_frame(self):
+        self.parser.feed("<root>")
+
+        while True:
+            try:
+                data = self.sock.recv(1)
+                data = data.decode("utf-8")
+                self.parser.feed(data)
+                for event, elem in self.parser.read_events():
+                    if elem.tag == 'XCTIP':
+                        #print("READ FRAME",elem)
+                        ET.dump(elem)
+                        return elem
+            except ET.ParseError as e:
+                pass
+
+class SilicanConnectionThread(SilicanThreadBase):
+    def run(self):
+        self.parser = ET.XMLPullParser(['end'])
+        self.connect()
+        self.login()
+        self.register_req()
+
+        while True:
+            elem = self.read_frame()
+            change = elem.findall(".//Change_EV")
+
+            for row in change:
+                calls_state = row.find(".//CallsState").text
+                cr = row.find(".//CR").text
+
+                calling = "Nie wykryto numeru"
+                if row.find(".//Calling/Number") is not None:
+                    calling = row.find(".//Calling/Number").text
+                    if calling == "0":
+                        calling = "Nie wykryto numeru"
+
+                called = 0
+                if row.find(".//Called/Number") is not None:
+                    called = row.find(".//Called/Number").text
+
+                if calls_state == "NewCall_ST":
+                    self._signal.emit((config.SILICAN_CONNECTION,calling))
+                    user = db.find_user(str(calling))
+                    if user:
+                        self._signal.emit((config.SILICAN_USER_FOUND,user))
+
+                if calls_state == "Release_ST":
+                    self._signal.emit((config.SILICAN_RELEASE,''))
+        
